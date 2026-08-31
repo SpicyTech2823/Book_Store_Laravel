5053a37bad0c5c16d0443aac21a343b63da471a6
SpicyTech2823 <sakirinsles@gmail.com>
2026-08-31 22:54:30 +0700

    update

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-#!/bin/bash
+
+set -e
 
 echo "Clearing Laravel cache..."
+
 php artisan optimize:clear
 
-echo "Running database migrations..."
-php artisan migrate --force
-
 echo "Starting Apache..."
+
 exec apache2-foreground
